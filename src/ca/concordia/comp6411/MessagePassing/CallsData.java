f6e7b341c8ef664facc1f1d7098581d923c2832a
piyushthummar <piyushthummar305@gmail.com>
2020-06-19 15:00:02 -0400

    CallsData class to store as a structure is created

---
+++ src/ca/concordia/comp6411/MessagePassing/CallsData.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+package ca.concordia.comp6411.MessagePassing;
+
+import java.util.Arrays;
+
+/**
+ * @author PIYUSH
+ *
+ */
+public class CallsData {
+	private String sender;
+	private String[] receivers;
+	public String getSender() {
+		return sender;
+	}
+	public void setSender(String sender) {
+		this.sender = sender;
+	}
+	public String[] getReceivers() {
+		return receivers;
+	}
+	public void setReceivers(String[] receivers) {
+		this.receivers = receivers;
+	}
+	@Override
+	public String toString() {
+		return sender + ": " + Arrays.toString(receivers);
+	}
+	
+}
